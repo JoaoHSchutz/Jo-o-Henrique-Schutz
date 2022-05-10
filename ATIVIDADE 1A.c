d2d1ea056c42fd9e256f037c7c90575bff823999
João Henrique Schutz <joao_henrique_schutz2002@outlook.com>
2022-05-10 01:59:27 -0300

    Arquivo atividade 1

---
+++ ATIVIDADE 1A.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*O que o código está implementando: Ele solicita ao usuario uma categoria 
+e uma quantidade para essa categoria e depois calcula a média dos valores.*/
+int main (void)
+{
+  char categoria;
+  float media, quantidade, soma, total;
+  do
+  {
+      printf("Informe a categoria: ");
+      scanf("%c", &categoria);
+      if (categoria != 'A' && categoria != 'B' && categoria != 'C')
+      {
+        break;
+      }
+      printf("Informe a quantidade: ");
+      scanf("%f", &quantidade);
+      getchar();
+      
+      soma = soma + quantidade;
+      total++;
+      media = soma / total;
+  }
+  while(categoria == 'A' || categoria == 'B' || categoria == 'C');
+
+  printf("A media dos produtos e %.2f", media);
+
+  return 0;
+}
